1ecc52604aa4c11468cd6f6bf4df6c77f1bc9fa3
Jesper Wendel Devantier <code@pseudonyous.me>
2019-05-09 22:23:07 +0200

    rewrote parser file * uses structs for snippet/cstr (cstring) * uses Python class implemented in C for Parser (ParserState)
    * easier to clean up allocations (__dealloc__ in ParserState)
    * ParserState holds all allocations (cstr's can be reallocated
      if input is too large, but it shouldn't happen often)

---
+++ pyt/parser/parser.pxd
@@ -0,0 +1,62 @@
+# cython: language_level=3
+from libc.stdio cimport FILE
+import typing as t
+ctypedef Py_UNICODE wchar_t
+
+# cdef class CString:
+#     cdef size_t buflen
+#     cdef wchar_t *ptr
+#     cdef size_t strlen
+#
+#
+#     cdef int ncpy_wchar(self, wchar_t *src, size_t n)
+#     cdef int ncpy_unicode(self, unicode s, size_t n)
+#     cdef void reset(self)
+#     cdef size_t len(self)
+#     cdef unicode repr(self)
+
+cdef struct cstr:
+    size_t buflen
+    wchar_t *ptr
+    size_t strlen
+
+cdef enum SNIPPET_TYPE:
+    SNIPPET_NONE
+    SNIPPET_OPEN
+    SNIPPET_CLOSE
+
+cdef struct snippet:
+    cstr *cstr
+    SNIPPET_TYPE type
+    size_t line_num
+
+
+cdef class Parser:
+    # file handlers for input and output files
+    cdef FILE *fh_in
+    cdef FILE *fh_out
+
+    # buffer holding the name of the temporary file made iff
+    # input and eventual output file are the same (in-place writing)
+    cdef cstr *tmp_file_path
+
+    # buffers holding the literals identifying the tags
+    # opening and ending a snippet (e.g. '<@@' and '@@>' respectively)
+    cdef cstr *tag_open
+    cdef cstr *tag_close
+
+    # buffers for holding information on the snippet start/end tags
+    # (name, line number...)
+    # TODO: change to Snippet class
+    cdef snippet *snippet_start
+    cdef snippet *snippet_end
+
+    # buffer holding a line of input from fh_in
+    cdef cstr *line
+    cdef size_t line_num
+
+
+    cdef repr(self)
+    cdef int snippet_find(self, snippet* dst)
+    cdef int readline(self)
+
--- pyt/parser/parser.pyx
+++ pyt/parser/parser.pyx
@@ -48,87 +48,160 @@ def tmp_file(path):
     tf.close()
     return fname
 
+class PytError(Exception):
+    pass
+
 ################################################################################
 ## CString
 ################################################################################
-cdef class CString:
+cdef struct cstr:
+    size_t buflen
+    wchar_t *ptr
+    size_t strlen
 
-    def __init__(self, size_t initial_size):
-        if initial_size == 0:
-            raise ValueError("initial_size must be a positive integer")
-        initial_size = initial_size + 1 # reserve space for null character
+cdef cstr *cstr_new(size_t initial_size):
+    cdef cstr *self = NULL
+    if initial_size == 0:
+        raise ValueError("initial_size must be a positive integer")
+    initial_size = initial_size + 1 # reserve space for null character
 
-        self.ptr = NULL
-        self.ptr = <wchar_t *> malloc(initial_size * sizeof(wchar_t))
-        if self.ptr == NULL:
-            raise MemoryError("cannot allocate string buffer")
-        wmemset(self.ptr, '\0', 1) # terminate "string"
-        self.buflen = initial_size
-        self.strlen = 0
+    self = <cstr *>malloc(sizeof(cstr))
+    if self == NULL:
+        return NULL
 
-    def __dealloc__(self):
-        if self.ptr != NULL:
-            free(self.ptr)
+    self.ptr = NULL
+    self.ptr = <wchar_t *> malloc(initial_size * sizeof(wchar_t))
+    if self.ptr == NULL:
+        free(self)
+        return NULL
+    wmemset(self.ptr, '\0', 1) # terminate "string"
+    self.buflen = initial_size
+    self.strlen = 0
+    return self
 
-    cdef int ncpy_wchar(self, wchar_t *src, size_t n):
-        cdef wchar_t *new_ptr = NULL
-        if n == 0:
-            n = wcslen(src) # does NOT include null terminator
-        if n >= self.buflen:
-            new_ptr = <wchar_t *> realloc(self.ptr, (n + 1) * sizeof(wchar_t))
-            if new_ptr == NULL:
-                return -1
-            self.buflen = n + 1
-            self.ptr = new_ptr
-        memcpy(self.ptr, src, n * sizeof(wchar_t))
-        self.ptr[n] = '\0'
-        self.strlen = n
-        return 0
+cdef void cstr_free(cstr *self):
+    if self == NULL:
+        return
+    if self.ptr != NULL:
+        free(self.ptr)
+    free(self)
 
-    cdef int ncpy_unicode(self, unicode s, size_t n):
-        cdef:
-            wchar_t *src = s
-        return self.ncpy_wchar(src, n)
+cdef int cstr_ncpy_wchar(cstr *self, wchar_t *src, size_t n):
+    cdef wchar_t *new_ptr = NULL
+    if n == 0:
+        n = wcslen(src) # does NOT include null terminator
+    if n >= self.buflen:
+        new_ptr = <wchar_t *> realloc(self.ptr, (n + 1) * sizeof(wchar_t))
+        if new_ptr == NULL:
+            return -1
+        self.buflen = n + 1
+        self.ptr = new_ptr
+    memcpy(self.ptr, src, n * sizeof(wchar_t))
+    self.ptr[n] = '\0'
+    self.strlen = n
+    return 0
 
-    cdef void reset(self):
-        self.ptr[0] = '\0'
-        self.strlen = 0
+cdef int cstr_ncpy_unicode(cstr *self, unicode s, size_t n):
+    cdef:
+        wchar_t *src = s
+    return cstr_ncpy_wchar(self, src, n)
 
-    cdef size_t len(self):
-        return self.strlen
+cdef void cstr_reset(cstr *self):
+    self.ptr[0] = '\0'
+    self.strlen = 0
 
-    cdef unicode repr(self):
-        return (
-            "#CString["
-            "buflen: {}, "
-            "strlen: {}, "
-            "ptr: {}"
-            "]"
-        ).format(self.buflen, self.strlen, self.ptr)
+cdef size_t cstr_len(cstr *self):
+    return self.strlen
 
-    def __repr__(self):
-        return self.repr()
-
-# TODO: consider rewriting CString back to pure C
-#       It's OK that ParserState triggers some Python - but CString-interaction
-#       Bascially locks down the GIL now.
-#
-#       Also, having the top-level object (ParserState) retain all references
-#       still means that cleanup will become easy
+cdef unicode cstr_repr(cstr *self):
+    if self == NULL:
+        return "<null cstr*>"
+    return (
+        "#CString["
+        "buflen: {}, "
+        "strlen: {}, "
+        "ptr: {}"
+        "]"
+    ).format(self.buflen, self.strlen, self.ptr)
 
 ################################################################################
-## ParserState
+## Snippet
+################################################################################
+cdef struct snippet:
+    cstr *cstr
+    SNIPPET_TYPE type
+    size_t line_num
+
+cdef unicode snippet_type(SNIPPET_TYPE t):
+    if t == SNIPPET_NONE:
+        return "SNIPPET_NONE"
+    elif t == SNIPPET_OPEN:
+        return "SNIPPET_OPEN"
+    elif t == SNIPPET_CLOSE:
+        return "SNIPPET_CLOSE"
+    else:
+        return "UNKNOWN_SNIPPET"
+
+cdef snippet *snippet_new(size_t bufsiz):
+    cdef:
+        snippet *self = NULL
+        cstr *cstr = NULL
+
+    cstr = cstr_new(bufsiz)
+    if cstr == NULL:
+        return NULL
+
+    self = <snippet *>malloc(sizeof(snippet))
+    if self == NULL:
+        free(cstr)
+        return NULL
+    self.cstr = cstr
+    self.type = SNIPPET_NONE
+    self.line_num = 0
+    return self
+
+cdef void snippet_reset(snippet *self):
+    cstr_reset(self.cstr)
+    self.type = SNIPPET_NONE
+    self.line_num = 0
+
+cdef void snippet_free(snippet *self):
+    if self.cstr != NULL:
+        cstr_free(self.cstr)
+    free(self)
+
+cdef int snippet_cmp(snippet *lhs, snippet *rhs):
+    return wcscmp(lhs.cstr.ptr, rhs.cstr.ptr)
+
+cdef unicode snippet_repr(snippet *self):
+    if self == NULL:
+        return "<null snippet*>"
+    return (
+        "#snippet["
+        "cstr: {}, "
+        "type: {}, "
+        "line_num: {}"
+        "]"
+    ).format(cstr_repr(self.cstr), snippet_type(self.type), self.line_num)
+
+################################################################################
+## Parser
 ################################################################################
 DEF BUF_LINE_LEN = 512
 DEF BUF_SNIPPET_NAME_LEN = 80
 DEF BUF_INDENT_BY_LEN = 40
 
+cdef enum:
+    READ_EOF = -1
+    READ_OK = 0
+    READ_ERR = 1
+    READ_LINE_TOO_LONG = 2
+
 # TODO: is it OK that tag_open == tag_close (e.g. '@@')
-cdef class ParserState:
+cdef class Parser:
 
     def __init__(
             self,
-            fpath_src: str, fpath_dst: t.Optional[str],
             tag_open: str = '<@@', tag_close: str = '@@>',
             buf_len_line = BUF_LINE_LEN,
             buf_snippet_name_len = BUF_SNIPPET_NAME_LEN,
@@ -138,30 +211,50 @@ cdef class ParserState:
         if buf_len_line <= 0:
             raise ValueError("buf_len_line must be positive")
 
-        self.tmp_file_path = CString(250)
-        self.reset(fpath_src, fpath_dst)
+        #self.tmp_file_path = CString(250)
+        self.tmp_file_path = cstr_new(250)
+        if self.tmp_file_path == NULL:
+            raise MemoryError("allocating tmp_file_path")
 
         # tag_open (e.g. '<@@')
-        self.tag_open = CString(len(tag_open))
-        if self.tag_open.ncpy_unicode(tag_open, len(tag_open)) != 0:
+        self.tag_open = cstr_new(len(tag_open))
+        if self.tag_open == NULL:
+            raise MemoryError("allocating tag_open")
+        if cstr_ncpy_unicode(self.tag_open, tag_open, len(tag_open)) != 0:
             raise MemoryError("tag_open")
 
         # tag_close (e.g. '@@>')
-        self.tag_close = CString(len(tag_close))
-        if self.tag_close.ncpy_unicode(tag_close, len(tag_close)) != 0:
+        self.tag_close = cstr_new(len(tag_close))
+        if self.tag_close == NULL:
+            raise MemoryError("allocating tag_close")
+        if cstr_ncpy_unicode(self.tag_close, tag_close, len(tag_close)) != 0:
             raise MemoryError("tag_close")
 
         # snippet_start
-        self.snippet_start = CString(buf_snippet_name_len)
+        self.snippet_start = snippet_new(buf_snippet_name_len)
+        if self.snippet_start == NULL:
+            raise MemoryError("allocating snippet_start")
 
         # snippet_end
-        self.snippet_end = CString(buf_snippet_name_len)
+        self.snippet_end = snippet_new(buf_snippet_name_len)
+        if self.snippet_end == NULL:
+            raise MemoryError("allocating snippet_end")
 
         # line (buffer)
-        self.line = CString(buf_len_line)
+        self.line = cstr_new(buf_len_line)
+        if self.line == NULL:
+            raise MemoryError("allocating line")
 
     def reset(self, fpath_src: str, fpath_dst: t.Optional[str]):
-        # TODO: also reset lune number (where IS the line number?) and so on
+        # TODO: also reset line number (where IS the line number?) and so on
+        # If overwriting the input file - generate a tempfile for output
+        if not fpath_dst:
+            fpath_dst = tmp_file(fpath_src)
+            if cstr_ncpy_unicode(self.tmp_file_path, fpath_dst, len(fpath_dst)) != 0:
+                raise MemoryError("failed to copy string to tmp_file_path")
+
+        cstr_reset(self.line)
+
         # Close input file if necessary
         if self.fh_in != NULL:
             fclose(self.fh_in)
@@ -174,11 +267,6 @@ cdef class ParserState:
         if self.fh_in == NULL:
             raise FileNotFoundError(2, f"input file '{fpath_src}' not found")
 
-        # If overwriting the input file - generate a tempfile for output
-        if not fpath_dst:
-            fname_dst = tmp_file(fpath_src)
-            self.tmp_file_path = CString(len(fpath_dst))
-
         # Close output file if necessary
         if self.fh_out != NULL:
             fclose(self.fh_out)
@@ -190,9 +278,7 @@ cdef class ParserState:
         self.fh_out = fopen(fh_out_str, 'wb')
         if self.fh_out == NULL:
             # TODO: better error needed
-            raise RuntimeError(f"failed to open output file: '{fname_dst}'")
-
-
+            raise RuntimeError(f"failed to open output file: '{fpath_dst}'")
 
     def __dealloc__(self):
         if self.fh_in != NULL:
@@ -200,9 +286,25 @@ cdef class ParserState:
         if self.fh_out != NULL:
             fclose(self.fh_out)
 
+        if self.tmp_file_path != NULL:
+            cstr_free(self.tmp_file_path)
+
+        if self.tag_open != NULL:
+            cstr_free(self.tag_open)
+        if self.tag_close != NULL:
+            cstr_free(self.tag_close)
+
+        if self.snippet_start != NULL:
+            snippet_free(self.snippet_start)
+        if self.snippet_end != NULL:
+            snippet_free(self.snippet_end)
+
+        if self.line != NULL:
+            cstr_free(self.line)
+
     cdef repr(self):
         return (
-            "#ParserState["
+            "#Parser["
             "fh_in: {}, "
             "fh_out: {}, "
             "tmp_file_path: {}, "
@@ -212,16 +314,101 @@ cdef class ParserState:
         ).format(
             '<null FILE*>' if self.fh_in == NULL else 'FILE*',
             '<null FILE*>' if self.fh_out == NULL else 'FILE*',
-            self.tmp_file_path.repr() if self.tmp_file_path else '<null CString>',
-
-            self.tag_open.repr() if self.tag_open else '<null CString>',
-            self.tag_close.repr() if self.tag_close else '<null CString>',
-
-            self.snippet_start.repr() if self.snippet_start else '<null CString>',
-            self.snippet_end.repr() if self.snippet_end else '<null CString>',
-
-            self.line.repr() if self.line else '<null CString>',
+            cstr_repr(self.tmp_file_path),
+            cstr_repr(self.tag_open),
+            cstr_repr(self.tag_close),
+            snippet_repr(self.snippet_start),
+            snippet_repr(self.snippet_end),
+            cstr_repr(self.line)
         )
 
     def __repr__(self):
         return self.repr()
+
+    cdef int snippet_find(self, snippet* dst):
+        cdef:
+            wchar_t *start = NULL
+            wchar_t *end = NULL
+            SNIPPET_TYPE typ = SNIPPET_OPEN
+            int ret = 0
+        start = wcsstr(self.line.ptr, self.tag_open.ptr)
+        if start == NULL:
+            # print("snippet_find start")
+            return -1
+
+        # advance beyond the snippet prefix itself
+        start = start + self.tag_open.strlen
+
+        # tag sits at the end of the line-buffer, abort
+        if (start - self.line.ptr) >= self.line.buflen:
+            # print("snippet_find OOB")
+            return -1
+
+        if start[0] == '/': # end snippet, advance past slash, too
+            typ = SNIPPET_CLOSE
+            start = start +1
+
+        # find snippet suffix, use 'start' to enforce ordering of tags
+        # and limit the search scope.
+        end = wcsstr(start, self.tag_close.ptr)
+        if end == NULL:
+            # print("snippet_find end")
+            return -1
+
+        ret = cstr_ncpy_wchar(dst.cstr, start, end-start)
+        if ret != 0:
+            # print("snippet_find cpy")
+            return ret
+        dst.type = typ
+        dst.line_num = self.line_num
+        return 0
+
+    cdef int readline(self):
+        # Read new line into buffer, advance linenumber if OK
+        # signal errors otherwise (READ_ERR, READ_EOF)
+        cdef size_t n = 0
+        if not fgetws(self.line.ptr, self.line.buflen, self.fh_in):
+            if feof(self.fh_in):
+                # print("readline: READ_EOF")
+                return READ_EOF
+            # print("readline: READ_ERR")
+            return READ_ERR
+
+        self.line_num = self.line_num + 1
+        # print("readline: READ_OK")
+        return READ_OK
+
+    def parse(self, fname_src: str, fname_dst: t.Optional[str]):
+        cdef:
+            int ret = READ_OK
+        self.reset(fname_src, fname_dst)
+
+        setlocale(LC_ALL, "en_GB.utf8") # TODO: set differently ?
+        while True:
+            ret = self.readline()
+            if ret:
+                print("parse -- ITER BREAK")
+                break
+
+            if self.snippet_find(self.snippet_start) != 0:
+                continue
+            if self.snippet_start.type != SNIPPET_OPEN:
+                raise PytError(f"Expected SNIPPET_OPEN, got '{snippet_type(self.snippet_start.type)}'")
+
+            while True: # Got the opening snippet, look for closing snippet
+                ret = self.readline()
+                if ret:
+                    # TODO: investigate, is it OK to break and have outer loop
+                    #       attempt to read the line again even though EOF?
+                    break
+
+                if self.snippet_find(self.snippet_end) != 0:
+                    continue
+                if self.snippet_end.type != SNIPPET_CLOSE:
+                    raise PytError(f"Expected SNIPPET_OPEN, got '{snippet_type(self.snippet_end.type)}'")
+
+                # TODO: rewrite to snippet_cmp
+                if snippet_cmp(self.snippet_start, self.snippet_end) != 0:
+                    raise PytError("got open and close snippets referring to different snippets")
+
+                print(f"SNIPPET '{self.snippet_start.cstr.ptr}' from {self.snippet_start.line_num}-{self.snippet_end.line_num}")
