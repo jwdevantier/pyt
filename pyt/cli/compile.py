b1999660613d60b9fc7d6293548e4f9a6d8c0f1f
Jesper Wendel Devantier <code@pseudonyous.me>
2019-06-30 09:46:44 +0200

    compile: support single- and multicore compilation

--- pyt/cli/compile.py
+++ pyt/cli/compile.py
@@ -3,6 +3,8 @@ import os
 from re import compile as re_compile
 from watchdog.events import (FileModifiedEvent, FileMovedEvent, FileDeletedEvent)
 import typing as t
+from multiprocessing import cpu_count, Pipe, Process
+from multiprocessing.connection import Connection
 
 from pyt.utils import watch
 from pyt.parser import Parser, Context, PARSE_OK, parse_result_err
@@ -14,21 +16,22 @@ from pyt.cli.conf import Configuration
 log = logging.getLogger(__name__)
 
 
-# TODO: - scandir works, templating invoked .nothing is written to supposed output file
-
-
-def _file_map(fn: t.Callable, path: str, regex_match: t.Callable):
+def _file_map(fn: t.Callable, path: str, ignore_file: t.Callable, include_file: t.Callable, ):
     for entry in os.scandir(path):
         if entry.is_dir():
-            yield from _file_map(fn, entry.path, regex_match)
-        elif regex_match(entry.path):
+            yield from _file_map(fn, entry.path, ignore_file, include_file)
+        elif ignore_file(entry.path):
+            continue
+        elif include_file(entry.path):
             log.debug(f"match: '{entry.path}'")
             yield fn(entry)
 
 
-def file_map(fn: t.Callable, path: str, include_patterns: t.List[str]):
-    valid_file = re_compile('|'.join(f'(?:{entry})' for entry in include_patterns)).match
-    return _file_map(fn, path, valid_file)
+def file_map(fn: t.Callable, path: str, ignore_patterns: t.List[str], include_patterns: t.List[str]):
+    ignore_file = re_compile('|'.join(f'(?:{entry})' for entry in ignore_patterns)).match
+    include_file = re_compile('|'.join(f'(?:{entry})' for entry in include_patterns)).match
+
+    return _file_map(fn, path, ignore_file, include_file)
 
 
 def expand_snippet(ctx: Context, snippet: str, prefix: str, out: IWriter):
@@ -67,7 +70,8 @@ class CompileWatcher(watch.Watcher):
         self._file_hashes.pop(event.src_path, None)
 
 
-def compile_once(config: Configuration) -> None:
+def compile_once_singlecore(config: Configuration) -> None:
+    log.info("compile_once_singlecore selected")
     parser = Parser(config.parser.open, config.parser.close)
     fid = 0
 
@@ -84,8 +88,51 @@ def compile_once(config: Configuration) -> None:
             log.debug(f"parse() => {out} {file_hash(out_path)}")
 
     log.info(f"compile path '{config.project.absolute().as_posix()}'")
-    list(file_map(parse_file, config.project.absolute().as_posix(), config.parser.include_patterns))
+    list(file_map(
+        parse_file, config.project.absolute().as_posix(),
+        config.parser.ignore_patterns, config.parser.include_patterns))
     print(f"parsed {fid} files.")
+
+
+def compile_once_mp(config: Configuration) -> None:
+    log.info("compile_once_mp selected")
+    cpus = config.parser.cores
+
+    def target(src: Connection):
+        parser = Parser(config.parser.open, config.parser.close)
+        fpath: t.Optional[str] = src.recv()
+        while fpath:
+            # print(f"got item '{fpath}'")
+            out = parser.parse(expand_snippet, fpath, None)
+            if out != 0:
+                print(f"Parse error")
+            fpath = src.recv()
+        # exits on encountering 'None'
+
+    procs = []
+    src_pipes = []
+    for n in range(cpus):
+        src, dst = Pipe()
+        proc = Process(target=target, args=(dst,))
+        proc.daemon = True
+        proc.start()
+        dst.close()
+
+        procs.append(proc)
+        src_pipes.append(src)
+
+    files = file_map(
+        lambda entry: entry.path, config.project.absolute().as_posix(),
+        config.parser.ignore_patterns, config.parser.include_patterns)
+
+    def current_pipes_gen() -> t.Iterator[Connection]:
+        while True:
+            for pipe in src_pipes:
+                yield pipe
+
+    src_gen = current_pipes_gen()
+    for entry_path in files:
+        next(src_gen).send(entry_path)
 
 
 def compile_watch(config: Configuration):
@@ -94,8 +141,12 @@ def compile_watch(config: Configuration):
 
 
 def compile(config: Configuration, watch: bool) -> None:
-    log.info(f"compile mode: '{'watch' if watch else 'once'}'")
-    # TODO: re-enable the compile-once step
-    compile_once(config)
+    log.info(f"compile mode: '{'watch' if watch else 'once'}' with {config.parser.cores} CPU cores")
+    log.info(f"compile path '{config.project.absolute().as_posix()}'")
+    if config.parser.cores == 1:
+        compile_fn = compile_once_singlecore
+    else:
+        compile_fn = compile_once_mp
+    compile_fn(config)
     if watch:
         compile_watch(config)
