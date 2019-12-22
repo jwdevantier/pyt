026ef791845d077f5b5e87c283cfb619f0ed444b
Jesper Wendel Devantier <code@pseudonyous.me>
2019-12-22 17:02:58 +0100

    visitor pattern + ast rewriter for component blocks

---
+++ ghostwriter/utils/cogen/visitor.py
@@ -0,0 +1,74 @@
+from typing import Callable, List
+from ghostwriter.utils.cogen.parser import (
+    Program, Block, If, Literal, Expr, Line, CLine, Node
+)
+import re
+
+rgx_fn_call = re.compile(r"(?P<ident>^[^\d\W]\w*)\((?P<args>.*)\)\Z", re.UNICODE)
+
+
+class Visitor:
+
+    def visit(self, node):
+        return getattr(self,
+                       f'visit_{type(node).__name__}',
+                       self.default_visitor)(node)
+
+    def default_visitor(self, node):
+        raise RuntimeError(f"no visitor for nodes of type '{type(node).__name__}'")
+
+
+class ASTVisitor(Visitor):
+    def visit_Program(self, node: Program):
+        return Program([self.visit(l) for l in node.lines])
+
+    def visit_Block(self, node: Block):
+        return Block(
+            self.visit(node.header),
+            [self.visit(l) for l in node.lines]
+        )
+
+    def visit_If(self, node: If):
+        return If([self.visit(cond) for cond in node.conds])
+
+    def visit_Literal(self, node: Literal):
+        return node
+
+    def visit_Expr(self, node: Expr):
+        return node
+
+    def visit_Line(self, node: Line):
+        return Line([self.visit(n) for n in node.contents])
+
+    def visit_CLine(self, node: CLine):
+        return node
+
+
+class Component(Node):
+    def __init__(self, identifier: str, args: str, lines: List[Node]):
+        self.identifier = identifier
+        self.args = args
+        self.lines = lines
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, self.__class__)
+            and other.identifier == self.identifier
+            and other.args == self.args
+            and other.lines == self.lines
+        )
+
+    def __repr__(self):
+        return f'r({self.identifier}: ~~{self.args}~~, {self.lines})'
+
+
+class RewriteComponentNodes(ASTVisitor):
+    def visit_Block(self, node: Block):
+        if node.header.keyword != 'r':
+            return super().visit_Block(node)
+        m = rgx_fn_call.match(node.header.args)
+        if not m:
+            raise RuntimeError("Invalid component")
+        return Component(m.group('ident'), m.group('args'), [
+            self.visit(l) for l in node.lines
+        ])
---
+++ tests/utils/cogen/test_visitor.py
@@ -0,0 +1,62 @@
+import pytest
+from ghostwriter.utils.cogen.tokenizer import (
+    PyTokenFactory as TokenFactory,
+    Tokenizer
+)
+from ghostwriter.utils.cogen.parser import (
+    CogenParser, Program, Literal, Expr, Line, CLine, Block, If,
+    ParserError, UnexpectedTokenError, InvalidBlockNestingError, InvalidEndBlockArgsError
+)
+from ghostwriter.utils.cogen.visitor import RewriteComponentNodes, Component
+
+
+@pytest.mark.parametrize("msg, prog, result", [
+    ("component-block",
+     "\n".join([
+         "%r MyFN(self.fn_name, self.fn_args)",
+         'print("hello, world")',
+         "%/r",
+     ]),
+     Program([
+         Component(
+             'MyFN', 'self.fn_name, self.fn_args', [
+                Line([Literal('print("hello, world")')])
+             ])
+     ])),
+
+    ("nested component-blocks",
+     "\n".join([
+         "%r MyFN(self.fn_name, self.fn_args)",
+         'print("hello, world")',
+         '%r MyInnerFN(self.fn_name, self.fn_args)',
+         'print("hello, from inside")',
+         "%/r",
+         "%/r",
+     ]),
+     Program([
+         Component(
+             'MyFN', 'self.fn_name, self.fn_args', [
+                Line([Literal('print("hello, world")')]),
+                Component('MyInnerFN', 'self.fn_name, self.fn_args', [
+                    Line([Literal('print("hello, from inside")')]),
+                ])
+             ])
+     ])),
+
+    ("non-component block",
+     "\n".join([
+         "%foo MyFN(self.fn_name, self.fn_args)",
+         'print("hello, world")',
+         "%/foo",
+     ]),
+     Program([
+         Block(CLine('foo', 'MyFN(self.fn_name, self.fn_args)'), [
+            Line([Literal('print("hello, world")')])
+         ])
+     ])),
+])
+def test_component(msg, prog, result):
+    parser = CogenParser(Tokenizer(prog))
+    v = RewriteComponentNodes()
+
+    assert v.visit(parser.parse_program()) == result, f"failed: {msg}"
