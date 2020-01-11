055167bbebe0e89f5820fa6f96ab62a550130166
Jesper Wendel Devantier <code@pseudonyous.me>
2020-01-11 11:03:05 +0100

    testlib.programs: store blocks in Examples, more testcases

--- testlib/programs.py
+++ testlib/programs.py
@@ -4,13 +4,15 @@ from ghostwriter.utils.cogen.parser import (
 )
 
 Scope = Dict[str, Any]
+Blocks = Dict[str, Any]
 
 
 class Example:
-    def __init__(self, header: str, scope: Scope, result: List[str], ):
+    def __init__(self, header: str, result: List[str], scope: Scope, blocks: Blocks = None):
         self.header = header
-        self.scope = scope
         self.result = '\n'.join(result)
+        self.scope = scope
+        self.blocks = blocks or {}
 
 
 class TestCase:
@@ -21,7 +23,7 @@ class TestCase:
         self.examples = examples
 
 
-line_literal = TestCase(
+line_literal_simplest = TestCase(
     "line - single literal",
     [
         "hello, world\n"
@@ -31,10 +33,11 @@ line_literal = TestCase(
             Literal("hello, world")])]),
     Example(
         '',
-        {},
         [
             'hello, world\n'
-        ]))
+        ],
+        {},
+        ))
 
 line_lit_var = TestCase(
     "line - literal, multiple elements (& variable)",
@@ -48,16 +51,55 @@ line_lit_var = TestCase(
             Literal("!")])]),
     Example(
         '',
-        {'thing': 'world'},
         [
             'hello, world!\n'
-        ]),
+        ],
+        {'thing': 'world'}),
     Example(
         '',
-        {'thing': 'people'},
         [
             'hello, people!\n'
-        ]))
+        ],
+        {'thing': 'people'},))
+
+line_expr_first = TestCase(
+    "line starting with an expression",
+    [
+        "<<greeting>>"
+    ],
+    Program([
+        Line([
+            Expr("greeting")
+        ])
+    ]),
+    Example(
+        "",
+        [
+            "hello!"
+        ],
+        {"greeting": "hello!"}
+    )
+)
+
+line_lit_adv = TestCase(
+    "line - multiple literals and expressions",
+    [
+        "<<name>>: <<age>> years old"
+    ],
+    Program([
+        Line([
+            Expr("name"),
+            Literal(": "),
+            Expr("age"),
+            Literal(" years old")])]),
+    Example(
+        "",
+        [
+            "Peter: 12 years old"
+        ],
+        {"name": "Peter", "age": 12}
+    )
+)
 
 if_simplest = TestCase(
     "if-simplest",
@@ -71,17 +113,17 @@ if_simplest = TestCase(
             Line([Literal("x is something")])])])]),
     Example(
         "set x",
-        {"x": 1},
         [
-            "x is something"
-        ]
+            "x is something\n"
+        ],
+        {"x": 1},
     ),
     Example(
         "x is unset",
-        {"x": None},
         [
-            "\n"
-        ]
+            ""
+        ],
+        {"x": None},
     )
 )
 
@@ -105,24 +147,24 @@ if_elif_else = TestCase(
                 Line([Literal('meh, who cares')])])])]),
     Example(
         '',
-        {'foo': 1},
         [
             'foo won!\n'
-        ]),
+        ],
+        {'foo': 1},),
     Example(
         '',
-        {'foo': 2},
         [
             'foo got second place!\n'
-        ]),
+        ],
+        {'foo': 2}),
     Example(
         '',
-        {'foo': '3'},
         [
             'meh, who cares\n'
-        ]))
+        ],
+        {'foo': 3}))
 
-for_block = TestCase(
+for_block_simplest = TestCase(
     "for loop - block",
     [
         "%for x in y",
@@ -134,24 +176,63 @@ for_block = TestCase(
             CLine('for', 'x in y'), [
                 Line([Literal("something")])])]),
     Example(
-        '',
-        {'y': []},
+        'empty iterable',
         [
-            "\n"
-        ]),
+            ""
+        ],
+        {'y': []}),
     Example(
-        '',
-        {'y': [1]},
+        'single-item iterable',
         [
             "something\n"
-        ]),
+        ],
+        {'y': [1]}),
     Example(
-        '',
-        {'y': [1, 2]},
+        'two-item iterable',
         [
             "something\n",
             "something\n"
-        ]))
+        ],
+        {'y': [1, 2]}))
+
+
+for_block_use_var = TestCase(
+    "for loop - block",
+    [
+        "%for name, age in persons",
+        "<<name>>: <<age>> years old",
+        "%/for"
+    ],
+    # [
+    #     "%for name, age in persons",
+    #     "<<name>>",
+    #     "%/for"
+    # ],
+    Program([
+        Block(
+            CLine('for', 'name, age in persons'), [
+                Line([Expr("name"), Literal(": "), Expr("age"), Literal(" years old")])])]),
+    Example(
+        'empty iterable',
+        [
+            ""
+        ],
+        {'persons': []}),
+    Example(
+        'single-item iterable',
+        [
+            "Peter: 12 years old\n"
+        ],
+        {'persons': [("peter", 12)]}),
+    Example(
+        'two-item iterable',
+        [
+            "Janet: 38 years old\n",
+            "Peter: 12 years old\n"
+        ],
+        {'persons': [('Janet', 38), ('Peter', 12)]})
+)
+
 
 # TODO: need to embed / resolve components somehow (Add ComponentScope to examples)
 component_block = TestCase(
@@ -193,14 +274,14 @@ prog1 = TestCase(
     ]),
     Example(
         '',
-        {
-            'thing': 'clerk',
-            'y': [1, 2]
-        },
         [
             "hello, clerk!",
             "something",
             "else",
             "something",
             "else\n"
-        ]))
+        ],
+        {
+            'thing': 'clerk',
+            'y': [1, 2]
+        }))
